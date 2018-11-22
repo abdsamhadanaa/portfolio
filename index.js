b0f7c18e70ceec535313c477b5239c9c896730dc
ahmad <ahmadtayeb91@gmail>
2018-11-22 20:45:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function display(){
+  let m =  document.getElementById("motivation");
+
+  if(m.style.display==='none'){
+  motivation.style.display = "block";
+}else {
+  m.style.display = "none";
+}
+}
